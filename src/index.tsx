ab4e284c324c364afd2fedcf3cb6678c5d010f27
Arutyunyan Artyom <artalar@ya.ru>
2021-02-10 12:38:58 +0000

    improve bundlesize

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
 
-const cid = (Date.now() + Math.random() * 1e17).toString(36)
+const cid = Math.random().toString(36).slice(-4)
 let i = 0
-function createUid() {
-  return `stylerun_${cid}_${++i}`
+function createUid(name: string | number = ++i) {
+  return `sr_${name}_${cid}`
 }
-
-let scopeI = 0
-let ID: null | string = null
 
 export function stylerun<T extends keyof JSX.IntrinsicElements>(
   tag: T,
@@ -16,26 +13,19 @@ export function stylerun<T extends { className?: string; [K: string]: any }>(
   component: React.FunctionComponent<T>,
 ): React.FunctionComponent<T> & { className: string }
 export function stylerun(
-  Component_: keyof JSX.IntrinsicElements | React.FunctionComponent,
+  component: keyof JSX.IntrinsicElements | React.FunctionComponent,
 ): React.FunctionComponent & { className: string } {
-  if (typeof Component_ === `string`) {
-    return stylerun(props => <Component_ {...props} />)
+  if (typeof component === `string`) {
+    return stylerun((props) => React.createElement(component, props))
   }
 
   function Component(props: any, ref: any) {
-    try {
-      scopeI = 0
-      // `className` may be rewrited in built time for better debug or SSR
-      ID = Component.className.slice(1)
-      return (Component_ as React.FunctionComponent)(
-        Object.assign({}, props, {
-          className: `${ID} ${props.className || ``}`,
-        }),
-        ref,
-      )
-    } finally {
-      ID = null
-    }
+    return (component as React.FunctionComponent)(
+      Object.assign({}, props, {
+        className: `${Component} ${props.className || ``}`.slice(1),
+      }),
+      ref,
+    )
   }
   Component.className = `.${createUid()}`
   Component.toString = () => Component.className
@@ -44,7 +34,7 @@ export function stylerun(
 }
 
 export const StylerunContext = React.createContext(
-  new Map<string, { count: number; id: string }>(),
+  new Map<string, { count: number; id: string; innerHTML: string }>(),
 )
 
 export function Style({ children: style }: { children: string }) {
@@ -59,22 +49,17 @@ export function Style({ children: style }: { children: string }) {
         (meta = {
           count: 1,
           id: createUid(),
-        }),
-      )
-
-      document.head.append(
-        Object.assign(document.createElement(`style`), meta, {
-          type: `text/css`,
           innerHTML: style,
         }),
       )
+
+      document.head.append(Object.assign(document.createElement(`style`), meta))
     }
 
     return () => {
-      const meta = cache.get(style)!
-      if (--meta.count === 0) {
+      if (--meta!.count === 0) {
         cache.delete(style)
-        document.getElementById(meta.id)!.remove()
+        document.getElementById(meta!.id)!.remove()
       }
     }
   }, [cache, style])
@@ -82,19 +67,13 @@ export function Style({ children: style }: { children: string }) {
   return null
 }
 
-export function useCssVar(value: string | number, name = `var`) {
-  const nameRef = React.useRef<string>()
-  if (!nameRef.current) {
-    if (ID === null) {
-      throw new Error(`Can't use \`useCssVar\` outside Stylerun HOC`)
-    }
-    nameRef.current = `--${name}_${++scopeI}_${ID}`
-  }
+export function useCssVar(value: string | number, description = `var`) {
+  const name = React.useState(() => `--${createUid(description)}`)[0]
 
   return {
-    var: `var(${nameRef.current})`,
+    var: `var(${name})`,
     style: {
-      [nameRef.current]: value,
+      [name]: value,
     },
   }
 }
